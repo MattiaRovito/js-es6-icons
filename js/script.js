be5b3b342abd0ce9c29cb81d3a84ebc769f916d5
MattiaR <rovitomattia91@gmail.com>
2021-06-19 10:32:11 +0200

    esercizio incompleto

--- js/script.js
+++ js/script.js
@@ -1,8 +1,5 @@
 //! Milestone 1 Partendo dalla seguente struttura dati, mostriamo in pagina tutte le icone disponibili come da layout.
 
-//! Milestone 2 Coloriamo le icone per tipo.
-
-//! Milestone 3 Creiamo una select con i tipi di icone e usiamola per filtrare le icone.
 
 
 const icons = [
@@ -106,56 +103,106 @@ const icons = [
 
 
 
-// icons.forEach((element) => {
-
-//     document.getElementById('icone').innerHTML +=
-//     `
-//     <div class="box"><i class="${element.family} ${element.prefix}${element.name}"><i><br>
-//     ${element.name}</div>
-//     `
-    
-//     // console.log(
-//     //     `
-//     //     <i class="${element.family} ${element.prefix}${element.name}"><i>
-//     //     `
-//     // );
-// });
-
-// icons.forEach((element) => {
-
-//     const {name, prefix, family} = element;
-
-
-//     const elementHTML = 
-//     `
-//     <div>
-//         <i class="${family} ${prefix}${name}"><i>
-//         <div class="title">${name.toUpperCase()}</div>
-//     </div>
-//     `;
-    
-//     document.getElementById('icone').innerHTML += elementHTML;
-
-
-// });
-
 //! Richiamiamo la funzione 
-print(icons);
+// print(icons);
 
 
-//! Possiamo mettere tutto in una funzione
 
-function print(array){
+
+//! Milestone 2 Coloriamo le icone per tipo.
+
+
+//TODO abbiamo mappato l'array di prima in un altro array dove poter dare il colore
+
+const coloredArray = icons.map((element) =>{
+    // mettiamo un colore di default
+    let color = "purple";
+    if(element.type == "animal"){
+        color = "blue";
+    } else if (element.type == "vegetable"){
+        color = "orange"
+    }
+
+
+    //? deve restituire tutti gli elementi di prima + il colore.
+    //* con il return vogliamo un array con tutti gli oggetti
+    return {
+        ...element,
+        color : color 
+        // oppure solo 
+        // color
+    }
+});
+console.log(coloredArray);
+
+
+// ora richiamiamo il coloredArray, commentando il primo print();
+
+printIcons(coloredArray);
+
+
+
+
+
+
+
+//! Milestone 3 Creiamo una select con i tipi di icone e usiamola per filtrare le icone.
+
+const types = [];
+
+coloredArray.forEach((element) =>{
+    if(!types.includes(element.type)){
+    types.push(element.type);
+    document.getElementById('type').innerHTML += 
+    `
+    <option value="All">${element.type}</option>
+    `
+    }
+});
+console.log(types);
+
+const select = document.getElementById('type');
+
+
+select.addEventListener("change", function(){
+
+    const valoreSelect = select.value;
+    console.log(valoreSelect);
+    const filteredIcons = coloredArray.filter((element)=>{
+        return element.type == valoreSelect;
+    });
+
+    printIcons(filteredIcons);
+    if(valoreSelect =="All"){
+        printIcons(coloredArray);
+    }
+});
+
+
+
+
+
+
+
+
+
+// FUNZIONI
+
+
+function printIcons(array){
+
+    document.getElementById('icone').innerHTML ="";
 
     array.forEach((element) => {
+        
 
-        const {name, prefix, family} = element;
+        const {color, name, prefix, family} = element;
     
     
         const elementHTML = 
         `
-        <div>
-            <i class="${family} ${prefix}${name}"><i>
+        <div class="col-2 icone text-center " >
+            <i class="${family} ${prefix}${name}" style="color:${color}"><i>
             <div class="title">${name.toUpperCase()}</div>
         </div>
         `;
@@ -163,3 +210,5 @@ function print(array){
         document.getElementById('icone').innerHTML += elementHTML;
     });
 }
+
+
